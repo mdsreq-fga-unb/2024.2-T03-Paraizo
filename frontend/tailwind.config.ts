6be9bb2f58ec3923d91f1ae9f30b9022ef54ccdc
maykon.soares <maykon.soares@sicoob.com.br>
2025-01-09 09:42:27 -0300

    add: adicionando estrutura de pastas do frontend

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -11,6 +11,9 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        paraizo: {
+          cyan: "#739DAB" 
+        }
       },
     },
   },
